4ea1003cfaf6471a3a332b13ed2c108abbf13d07
Abhinav Sobti <sobtiabhinav@gmail.com>
2013-07-18 19:06:34 +0530

    finalised programs and changed logo

--- app/view/Home.js
+++ app/view/Home.js
@@ -11,11 +11,12 @@ Ext.define('DS.view.Home', {
 		
 		html: [
 			'<br /><img height=200 src="resources/icons/logo.png" />',
-			'<h1>DS Programs</h1>',
-			"<br /><p>Contains a few of the most widely used Data Structure programs in C</p>",
+			'<h1></h1>',
+			"<br /><p>Contains over 40 among the most widely used Data Structure programs in C <br /></p>",
+			"<p> The application finds its use mostly during your practicals (If you know what we mean :P)</p>",
 			"<p>You may request programs at <a>app@inkmagik.com</a>.</p>",
-			"<p>Please rate us if you like the app!</p>",
-			'<br /><h2>Developed by <a href="http://inkmagik.com" target="_blank">InkMagik</a></h2>'
+			"<p>Don't forget to rate us if you like the app!</p>",
+			'<br /><h2><b>Developed by <a href="http://inkmagik.com">InkMagik</a></b></h2>'
 		].join("")
 		
 		}	
